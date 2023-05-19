39d23c599134b4267901bc4d91bf65df15cc9902
Lari Löfman <larwazor@gmail.com>
2023-05-19 16:26:56 +0300

    add engine sound, fix leaving scene

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,3 +1,4 @@
+using FMOD.Studio;
 using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,12 +6,14 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    private List<EventInstance> eventInstances = new List<EventInstance>();
     public static AudioManager Instance { get; private set; }
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        if(Instance == null)
+        if(Instance != null)
         {
+            Instance.CleanUp();
             Debug.LogWarning("More than one AudioManager around!");
         }
         Instance = this;
@@ -19,5 +22,30 @@ public class AudioManager : MonoBehaviour
     public void PlayOneShot(EventReference sound, Vector3 worldPos)
     {
         RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+
+    public EventInstance CreateEventInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        eventInstances.Add(eventInstance);
+        Debug.Log("Add audio");
+        return eventInstance;
+    }
+
+    private void CleanUp()
+    {
+        foreach (EventInstance eventInstance in eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+
+        //eventInstances.Clear();
+        Debug.Log("CleanUp Audio");
+    }
+
+    private void OnDestroy()
+    {
+        //CleanUp();
     }
 }
--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -5,22 +5,19 @@ using UnityEngine;
 
 public class FMODEvents : MonoBehaviour
 {
+    [field: Header("Ship SFX")]
+    [field: SerializeField] public EventReference ShipEngine { get; private set; }
+
     [field: Header("Laser SFX")]
     [field: SerializeField] public EventReference LaserShoot { get; private set; }
     public static FMODEvents Instance { get; private set; }
 
-    void Start()
+    void Awake()
     {
-        if (Instance == null)
+        if (Instance != null)
         {
             Debug.LogWarning("More than one FMODEvents around!");
         }
         Instance = this;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
